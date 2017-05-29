d3636dd8cb4f3348ca182d9f66c7106527c74e2e
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2017-05-29 14:43:34 +0000

    BonusDrink.total_count_for を実装

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,14 @@
 class BonusDrink
+  BONUS_BY = 3
+
   def self.total_count_for(amount)
-    0
+    raise ArgumentError("negative amount: #{amount}") if amount < 0
+
+    bonus = amount / BONUS_BY
+    return amount if bonus == 0
+
+    remainings = amount % BONUS_BY
+    extra = total_count_for(bonus + remainings)
+    return (amount - remainings) + extra
   end
 end
